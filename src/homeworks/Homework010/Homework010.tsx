866f2d6df94c4b48bc61d0c91ae00a4ecabbf5d3
Evgen Grushchenko <evgen.tm959@gmail.com>
2024-07-17 20:27:25 +0200

    Homework010_2

--- src/homeworks/Homework010/Homework010.tsx
+++ src/homeworks/Homework010/Homework010.tsx
@@ -1,52 +1,59 @@
 import { ChangeEvent, useState } from "react";
-import { Homework010Component } from "./styles";
+import { Homework010Component, FormWrapper, ResultWrapper } from "./styles";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import ResultBlock from "../../components/ResultBlock/ResultBlock";
 
 function Homework010() {
-    // 1. Создаем состояние, в котором будет храниться актуальное значение поля
-    const [resultValue1, setResultValue1] = useState<string>('')
-    const [resultValue2, setResultValue2] = useState<string>('')
-    // 3. Создаём функцию, которая будет вызываться при изменении resultValue и изменять его состояние (значение)
-    const changeResultValue1 = (event: ChangeEvent<HTMLInputElement>) => {
-        setResultValue1(event.target.value)
-    }
-    const changeResultValue2 = (event: ChangeEvent<HTMLInputElement>) => {
-        setResultValue2(event.target.value)
-    }
+  const [resultValue1, setResultValue1] = useState<string>("");
+  const [resultValue2, setResultValue2] = useState<string>("");
+  const [value1Result, setvalue1Result] = useState<string>("");
+  const [value2Result, setvalue2Result] = useState<string>("");
+  const [isShowBlock, setIsShowBloc] = useState<boolean>(false);
 
-    return (
+  const changeResultValue1 = (event: ChangeEvent<HTMLInputElement>): void => {
+    console.log(event);
+    setResultValue1(event.target.value);
+  };
+
+  const changeResultValue2 = (event: ChangeEvent<HTMLInputElement>): void => {
+    setResultValue2(event.target.value);
+  };
+
+  const showResult = () => {
+    setvalue1Result(resultValue1);
+    setvalue2Result(resultValue2);
+    if (resultValue1!=='' || resultValue2!=='') {
+        setIsShowBloc(!isShowBlock)
+    }
+  };
+
+  return (
     <Homework010Component>
-        {/* 2. Передача значения в поле */}
+      <FormWrapper>
         <Input
-        label='Result 1'
-        name='result1'
-        placeholder='Enter result 1'
-        value={resultValue1}
-        // 4. Передача функции для изменения инпута resultValue
-        onChange={changeResultValue1} 
+          label="Result 1"
+          name="result1"
+          placeholder="Enter result 1"
+          value={resultValue1}
+          onChange={changeResultValue1}
         />
-        <Button
-        name='Result Value'
-        onButtonClick={()=> resultValue1 !=='' && <ResultBlock resultName='result 1' result={resultValue1}/>}
+        <Input
+          label="Result 2"
+          name="result1"
+          placeholder="Enter result 2"
+          value={resultValue2}
+          onChange={changeResultValue2}
         />
-         {/* 2. Передача значения в поле */}
-         <Input
-        label='Result 2'
-        name='result1'
-        placeholder='Enter result 2'
-        value={resultValue2}
-        // 4. Передача функции для изменения инпута resultValue
-        onChange={changeResultValue2} 
-        />
-        <Button
-        name='Result Value'
-        onButtonClick={()=> resultValue2 && <ResultBlock resultName='result 1' result={resultValue2}/>}
-        />
+        <Button name="Result Value 1" onButtonClick={showResult} />
+      </FormWrapper>
+      {isShowBlock && 
+        <ResultWrapper>
+          {value1Result !== '' && <ResultBlock resultName="result 1" result={value1Result} />}
+          {value2Result && <ResultBlock resultName="result 2" result={value2Result} />}
+        </ResultWrapper>}
     </Homework010Component>
-)
-    
+  );
 }
 
 export default Homework010;
--- src/homeworks/Homework010/styles.ts
+++ src/homeworks/Homework010/styles.ts
@@ -9,3 +9,18 @@ flex: 1;
 gap: 30px;
 padding: 200px;
 `
+export const FormWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 30px;
+min-width: 400px;
+max-width: 600px;
+padding: 20px;
+`
+
+export const ResultWrapper = styled.div`
+display: flex;
+align-items: center;
+gap: 30px;
+`
